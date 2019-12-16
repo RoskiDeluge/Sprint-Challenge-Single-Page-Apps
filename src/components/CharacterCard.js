6e014f564933816377f86a235bf80acea28e5da8
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2019-12-15 17:32:08 -1000

    First commit - succesful API request and display of characters

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { Card, CardHeader, CardBody, CardText, Col } from "reactstrap";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({character}) {
+  return (
+    <Col xs="6" md="4" lg="3">
+      <Card>
+        <CardHeader>{character.name}</CardHeader>
+        <CardBody>
+          <CardText>Status: {character.status}</CardText>
+          <CardText>Species: {character.species}</CardText>
+        </CardBody>
+      </Card>
+    </Col>
+  )
 }
