fbe8fa1ebc90c859c30e9edee2c1a8359f3b3de7
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2019-12-15 18:52:36 -1000

    Reached adding the Search component, but not functional

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CharacterCard from './CharacterCard';
 import { Container, Row } from "reactstrap";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([]);
+
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
@@ -23,17 +25,19 @@ export default function CharacterList() {
   }, []);
 
   return (
-    // <section className="character-list">
-    //   {/* {characters.map(character => {
-    //       return <CharacterCard character={character} key={character.id} />;
-    //     })} */}
-    //     {/* <h2>To do</h2> */}
-    // </section>
     <Container>
+      <Row><SearchForm /></Row>
       <Row>
         {characters.map(character => {
             return <CharacterCard character={character} key={character.id} />;
           })}
+        {/* <div>
+          <ul>
+            {searchResults.map(character => {
+              return <li key={character}>{character}</li>;
+            })}
+          </ul>
+        </div> */}
       </Row>
     </Container>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,30 @@
 import React, { useState } from "react";
 
 export default function SearchForm() {
+  const [searchTerm, setSearchTerm] = useState("");
+  // const [searchResults, setSearchResults] = useState(characters);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
  
   return (
     <section className="search-form">
-     // Add a search form here
+     <form>
+        {/* We apply two-way data binding to the input field, which basically takes the value from the user and saves it into the state. */}
+        {/* Two-way binding just means that:
+        When properties in the model get updated, so does the UI.
+        When UI elements get updated, the changes get propagated back to the model. */}
+        <label htmlFor="name">Search:</label>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="Search"
+          onChange={handleChange}
+          value={searchTerm}
+        />
+      </form>
     </section>
   );
 }
